9be8faa9d79ba41c04555e304f971c47b2617c0e
David Kurniawan <s160819001@student.ubaya.ac.id>
2022-02-13 21:33:41 +0700

    //registereditedbydk13022022213200

--- app/src/main/java/com/jitusolution/native_alfredo/Register.kt
+++ app/src/main/java/com/jitusolution/native_alfredo/Register.kt
@@ -4,4 +4,6 @@ class Register(var name: String, var username:String, var password:String){
     override fun toString(): String {
         return "$name $username $password"
     }
+
+    //registereditedbydk13022022213200
 }
