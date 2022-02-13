deef1f65d446f33c3bbdf813543ecda944f9f813
s160419006 <s160419006@student.ubaya.ac.id>
2022-02-13 21:47:04 +0700

    update variable names and add some register class

--- app/src/main/java/com/jitusolution/native_alfredo/Register.kt
+++ app/src/main/java/com/jitusolution/native_alfredo/Register.kt
@@ -3,5 +3,6 @@ package com.jitusolution.native_alfredo
 class Register(var name: String, var username:String, var password:String){
     override fun toString(): String {
         return "$name $username $password"
+        //ini comment dari register
     }
 }
